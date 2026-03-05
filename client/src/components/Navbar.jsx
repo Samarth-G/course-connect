5a61672d083c99019709454cea1ef1e7e1302366
Samarth Grover <samarthgrover8@gmail.com>
2026-03-05 01:16:01 -0800

    Navbar component added

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar() {
+  return (
+    <nav>
+      <a href="/">Home</a>
+      <a href="/courses">Courses</a>
+      <a href="/login">Login</a>
+    </nav>
+  )
+}
+export default Navbar
