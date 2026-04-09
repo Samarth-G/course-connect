800ea64514502a21346ad3ee8076b1fba7669adc
Samarth Grover <samarthgrover8@gmail.com>
2026-04-09 00:22:37 -0700

    Updated config and css

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,6 +10,10 @@ export default defineConfig({
         target: 'http://localhost:5050',
         changeOrigin: true,
       },
+      '/uploads': {
+        target: 'http://localhost:5050',
+        changeOrigin: true,
+      },
     },
   },
 })
